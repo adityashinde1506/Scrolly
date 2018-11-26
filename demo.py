a2c9f99b3f72a0bc46896e9d625bfe28dcd586ee
Aditya Shinde <shinde.aditya386@gmail.com>
2018-11-25 20:04:05 -0500

    basic player creation and playing done

--- demo.py
+++ demo.py
@@ -2,7 +2,30 @@ import os
 import time
 
 
-size = 10
+size = 50
+
+
+class Player:
+    """
+        Defines a basic player.
+    """
+    def __init__(self,
+                 name="unnamed",
+                 pos=0):
+
+        self.name = name
+        self.pos = pos
+
+    def play(self, game):
+        return 0
+
+
+def controller(game_object, game):
+
+    action = game_object.play(game)
+
+    if not action:
+        game_object.pos -= 1
 
 
 def init_scene():
@@ -16,19 +39,43 @@ def init_scene():
 def display_scene(scene):
     os.system("clear")
     print("Scrolly")
-    print("\r\n"*10)
+    print("\r\n"*5)
     print(" ".join(scene))
+
+
+def update_scene(game_objects):
+    """
+        Updates state according to actions.
+    """
+    scene = init_scene()
+    for obj in game_objects:
+        scene[obj.pos] = obj.name[0]
+
+    return scene
 
 
 def loop():
     """
         Defines main game loop.
     """
+    aditya = Player(pos=5, name="Aditya")
+    enemy = Player(pos=0, name="Enemy")
+    scene = init_scene()
+    objects = [aditya, enemy]
+
     while 1:
 
-        scene = init_scene()
-        display_scene(scene)
-        time.sleep(1)
+        for obj in objects:
+
+            # Control logic
+            controller(game_object=obj, game=scene[:])
+
+            # Update game
+            scene = update_scene(objects)
+
+            # Display logic
+            display_scene(scene)
+            time.sleep(1)
 
 
 if __name__ == "__main__":
